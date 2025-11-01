11a7f88e950616958be4439b9ce474ac8e676f80
deeph2203 <deeph3232@gmail.com>
2025-11-01 11:46:26 +0530

    basic js

---
+++ REACT/basic.js
@@ -0,0 +1,17 @@
+console.log("Hello World!");
+
+let nam ="deep";
+let age ="20";
+
+console.log(nam);
+console.log(age)
+
+console.log(nam,age);
+console.table([nam,age]);
+
+console.log(typeof nam);
+
+let a=2;
+let b="2";
+console.log(a==b);
+console.log(a===b);//
